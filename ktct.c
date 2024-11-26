80f2d8040ab649ce89cead181fa4ba02c85ffc04
ktct-h <c99447618@gmail.com>
2024-11-26 03:16:11 -0600

    I

---
+++ ktct.c
@@ -0,0 +1,11 @@
+#include "kitty.h"
+#include "bootstrap/booth.h"
+#include "room2b.h"
+kitten cat(age="1 months old") {
+	feed.food(getType)
+}
+catpos = kitten.catPos()
+if catPos > room.roomSize:
+	cat.block()
+else:
+break
